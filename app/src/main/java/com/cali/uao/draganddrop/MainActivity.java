49aef0772a949f2dcad5a97eb6c7afd54a0c18ef
kenosome <kennyj.osorio@gmail.com>
2016-04-23 11:30:56 -0500

    Drag and Drop con persistencia en las preferencias

--- app/src/main/java/com/cali/uao/draganddrop/MainActivity.java
+++ app/src/main/java/com/cali/uao/draganddrop/MainActivity.java
@@ -1,38 +1,91 @@
 package com.cali.uao.draganddrop;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    TextView cuadro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cuadro=(TextView) findViewById(R.id.cuadro);
+
+        DisplayMetrics dm=new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        cuadro.setOnTouchListener(new View.OnTouchListener() {
+
+            float xinicial;
+            float yinicial;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        cuadro.setBackgroundColor(Color.rgb(0xBB,0x66,0x66));
+                        xinicial=event.getX();
+                        yinicial=event.getY();
+
+                        return true;
+                    case MotionEvent.ACTION_MOVE:
+
+                        cuadro.setText("X: "+event.getX()+" \nY: "+event.getY());
+                        cuadro.setX(cuadro.getX()+event.getX()-xinicial);
+                        cuadro.setY(cuadro.getY()+event.getY()-yinicial);
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        cuadro.setBackgroundColor(Color.rgb(0xFF,0xAA,0xAA));
+
+                        float xfinal, yfinal;
+                        xfinal=cuadro.getX();
+                        yfinal=cuadro.getY();
+
+                        SharedPreferences preferencias= PreferenceManager
+                                .getDefaultSharedPreferences(getApplicationContext());
+
+                        SharedPreferences.Editor editor=preferencias.edit();
+
+                        editor.putFloat("X",xfinal);
+                        editor.putFloat("Y",yfinal);
+                        editor.commit();
+
+                        return true;
+                }
+
+                cuadro.setBackgroundColor(Color.rgb(0xEE,0x99,0x99));
+
+                return false;
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences preferencias = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        float Xrecuperado=preferencias.getFloat("X",0);
+        float Yrecuperado=preferencias.getFloat("Y",0);
+        cuadro.setX(Xrecuperado);
+        cuadro.setY(Yrecuperado);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
